799aeae2207b9b57883575131eccea741d351cf2
shreegowri <gaganandh@gmail.com>
2022-06-09 09:15:31 +0530

    [shreegowri] add new address to the address book

--- AdressBook/AdressBook/AdressBook.cs
+++ AdressBook/AdressBook/AdressBook.cs
@@ -6,10 +6,9 @@ using System.Threading.Tasks;
 
 namespace AdressBook
 {
-    internal class AdressBook
+    internal class AdressBook1
     {
-        public class Person
-        {
+        
             public string FirstName { get; set; }
             //The get method returns the value of the variable name//The set method assigns a value to the name variable
             public string LastName { get; set; }
@@ -19,6 +18,33 @@ namespace AdressBook
             public string Zip { get; set; }
             public string Email { get; set; }
             public string[] Addresses { get; set; }
-        }
+            public static List<AdressBook1> People = new List<AdressBook1>();
+
+            public static void AddPerson()
+            {
+            
+            AdressBook1 person = new AdressBook1();
+
+                Console.Write("Enter First Name: ");
+                person.FirstName = Console.ReadLine();
+                Console.Write("Enter Last Name: ");
+                person.LastName = Console.ReadLine();
+                Console.Write("Enter city name: ");
+                person.city = Console.ReadLine();
+                Console.Write("Enter zip number: ");
+                person.Zip = Console.ReadLine();
+                Console.Write("Enter Phone Number: ");
+                person.Email = Console.ReadLine();
+                Console.Write("Enter Phone Email: ");
+                person.PhoneNumber = Console.ReadLine();
+                Console.Write("Enter Address 1: ");
+                string[] addresses = new string[2];
+                addresses[0] = Console.ReadLine();
+                Console.Write("Enter Address 2 (Optional): ");
+                addresses[1] = Console.ReadLine();
+                person.Addresses = addresses;
+                People.Add(person);
+            }
+        
     }
 }
--- AdressBook/AdressBook/Program.cs
+++ AdressBook/AdressBook/Program.cs
@@ -2,10 +2,26 @@
 {
     class program
     {
+        
+
         public static void Main(string[] args)
         {
             Console.WriteLine("welcome to AdressBook");
+            string command = "";
+            while (command != "exit")
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter a command: ");
+                command = Console.ReadLine().ToLower();
+                switch (command)
+                {
+                    case "add":
+                        
+                        AdressBook1.AddPerson();
+                        break;
+                }
+            }
 
-        }
+                }
     }
 }
