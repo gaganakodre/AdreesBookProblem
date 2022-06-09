4960f6328587ccb76fe48b9b9ae3e5834b3e4606
shreegowri <gaganandh@gmail.com>
2022-06-09 08:44:17 +0530

    [shreegowri]creat contacts in Address Book

---
+++ AdressBook/AdressBook/AdressBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdressBook
+{
+    internal class AdressBook
+    {
+        public class Person
+        {
+            public string FirstName { get; set; }
+            //The get method returns the value of the variable name//The set method assigns a value to the name variable
+            public string LastName { get; set; }
+           
+            public string PhoneNumber { get; set; }
+            public string city  { get; set; }
+            public string Zip { get; set; }
+            public string Email { get; set; }
+            public string[] Addresses { get; set; }
+        }
+    }
+}
--- AdressBook/AdressBook/Program.cs
+++ AdressBook/AdressBook/Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("welcome to AdressBook");
+
         }
     }
 }
