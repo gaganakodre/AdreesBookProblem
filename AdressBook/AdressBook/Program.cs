7859293cd12474617231d02d028134cbd95663f6
shreegowri <gaganandh@gmail.com>
2022-06-09 08:30:40 +0530

    [shreegowri] welcome message

---
+++ AdressBook/AdressBook/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AdressBook
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to AdressBook");
+        }
+    }
+}
