29e83e76daa2a4468b693779955df629d3433f61
shreegowri <gaganandh@gmail.com>
2022-06-17 15:36:37 +0530

    [shreegowri]refactor add multiple address book to system and each book has unique name

--- AdressBook/AdressBook/Program.cs
+++ AdressBook/AdressBook/Program.cs
@@ -2,44 +2,76 @@
 {
     class program
     {
-
-
         public static void Main(string[] args)
         {
             Console.WriteLine("welcome to AdressBook");
-            AdressBook address = new AdressBook();
+            AdressBookSystem adressBookSystem = new AdressBookSystem();
+            string command1 = "";
 
-            string command = "";
-
-            while (command != "exit")
+            while (command1 != "exit")
             {
+                Console.WriteLine("enter the option: 1.Add AdressBook 2.Edit AddressBook 3.Remove AddressBook");
+                int option = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("Please enter a command add,edit,remove,display,multipleperson: ");
-                command = Console.ReadLine().ToLower();
-
-                switch (command)
+                switch (option)
                 {
-                    case "add":
+                    case 1:
+                        adressBookSystem.AddAddressBook();
+                        break;
+                    case 2:
+                        Console.WriteLine("enter the addressbook name you want to edit");
+                        string addressBookName = Console.ReadLine();
 
 
-                        address.AddPerson();
+                        if (adressBookSystem.adressBooks.ContainsKey(addressBookName.ToLower()))
+                        {
+
+
+                            string command = "";
+
+                            while (command != "exit")
+                            {
+
+                                Console.WriteLine("Please enter a command add,edit,remove,display,multipleperson: ");
+                                command = Console.ReadLine().ToLower();
+
+
+                                switch (command)
+                                {
+                                    case "add":
+
+
+                                        adressBookSystem.adressBooks[addressBookName.ToLower()].AddPerson();
+                                        break;
+                                    case "edit":
+                                        adressBookSystem.adressBooks[addressBookName.ToLower()].EditContact();
+                                        break;
+                                    case "remove":
+                                        adressBookSystem.adressBooks[addressBookName.ToLower()].RemoveContact();
+                                        break;
+                                    case "display":
+                                        adressBookSystem.adressBooks[addressBookName.ToLower()].Display();
+                                        break;
+                                    case "multipleperson":
+                                        adressBookSystem.adressBooks[addressBookName.ToLower()].AddMultipleContact();
+                                        break;
+                                    default:
+                                        command = "exit";
+                                        break;
+                                }
+                            }
+                        }
                         break;
-                    case "edit":
-                        address.EditContact();
+                    default:
+                        command1 = "exit";
                         break;
-                    case "remove":
-                        address.RemoveContact();
-                        break;
-                    case "display":
-                        address.Display();
-                        break;
-                    case "multipleperson":
-                        address.AddMultipleContact();
-                        break;
+
+
 
 
 
                 }
+
             }
         }
     }
